96d0866dbf4c3619b5db97de69a34eb91c1f9f4c
Craig R Webster <craig@barkingiguana.com>
2014-03-17 17:00:33 +0000

    More client project requirements

--- modules/barkingiguana/manifests/init.pp
+++ modules/barkingiguana/manifests/init.pp
@@ -1,12 +1,18 @@
 class barkingiguana {
   define project(
-    $nginx = undef,
-    $ruby  = undef
+    $nginx     = undef,
+    $ruby      = undef,
+    $redis     = undef,
+    $mysql     = undef,
+    $memcached = undef,
   ) {
     boxen::project { $name:
-      nginx  => $nginx,
-      ruby   => $ruby,
-      source => $name
+      nginx      => $nginx,
+      ruby       => $ruby,
+      redis      => $redis,
+      mysql      => $mysql,
+      memcached  => $memcached,
+      source     => $name
     }
   }
 }
