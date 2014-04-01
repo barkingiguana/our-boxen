bbfbe12bdb4c6b75fdcba1e6b008ac807fa5db3f
Craig R Webster <craig@barkingiguana.com>
2014-04-01 17:40:26 +0100

    Install Ruby 2.1.1

--- manifests/site.pp
+++ manifests/site.pp
@@ -67,9 +67,10 @@ node default {
   ruby::version { '1.9.3': }
   ruby::version { '2.0.0': }
   ruby::version { '2.1.0': }
+  ruby::version { '2.1.1': }
 
   class { 'ruby::global':
-    version => '2.1.0'
+    version => '2.1.1'
   }
 
   # common, useful packages
